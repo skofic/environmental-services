53e6a6da668203833d5b612f207e04232b6da36d
Milko Škofič <milko@skofic.io>
2023-08-28 19:45:44 +0200

    Finalised GCU data services.

--- main.js
+++ main.js
@@ -8,5 +8,6 @@ const { context } = require('@arangodb/locals')
 ///
 // Set routes.
 ///
+context.use('/gcu/data', require('./routes/unitsData'), 'Genetic Conservation Unit Data')
 context.use('/rs/data', require('./routes/remoteSensingData'), 'Remote Sensing Data')
 context.use('/rs/meta', require('./routes/remoteSensingMeta'), 'Remote Sensing Metadata')
