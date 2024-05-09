7c45a4e6bfb43f2e04f6daae29aace69c2a8af0c
Milko Škofič <milko@skofic.io>
2024-05-09 17:14:23 +0200

    Maybe found solution: let's cross fingers...

--- constants.js
+++ constants.js
@@ -22,8 +22,114 @@ const edgeCollections = {}
 // Views.
 ///
 const views = {
-	"shapes": "Shapes",
-	"dataset": "Dataset"
+	"shapes": {
+		"name": "VIEW_SHAPES",
+		"type": "arangosearch",
+		"properties": {
+			"links": {
+				"Shapes": {
+					"analyzers": [
+						"identity"
+					],
+					"fields": {
+						"_key": {},
+						"std_dataset_ids": {},
+						"geometry": {
+							"analyzers": [
+								"geojson"
+							]
+						},
+						"properties": {
+							"fields": {
+								"chr_AvElevation": {},
+								"chr_AvSlope": {},
+								"chr_StdElevation": {},
+								"geo_shape_area": {},
+								"chr_AvAspect": {}
+							}
+						},
+					},
+					"includeAllFields": false,
+					"storeValues": "none",
+					"trackListPositions": false
+				}
+			}
+		}
+	},
+	"dataset": {
+		"name": "VIEW_DATASET",
+		"type": "arangosearch",
+		"properties": {
+			"links": {
+				"Dataset": {
+					"analyzers": [
+						"identity"
+					],
+					"fields": {
+						"_key": {},
+						"count": {},
+						"_collection": {},
+						"_domain": {},
+						"_tag": {},
+						"_classes": {},
+						"_subject": {},
+						"_subjects": {},
+						"std_project": {},
+						"std_dataset_group": {},
+						"std_dataset": {},
+						"std_date": {},
+						"std_date_end": {},
+						"std_date_start": {},
+						"std_date_submission": {},
+						"std_terms": {},
+						"std_terms_key": {},
+						"std_terms_summary": {},
+						"std_terms_quant": {},
+						"subjects": {},
+						"_title": {
+							"fields": {
+								"iso_639_3_eng": {
+									"analyzers": [
+										"text_en",
+										"identity"
+									]
+								}
+							}
+						},
+						"_description": {
+							"fields": {
+								"iso_639_3_eng": {
+									"analyzers": [
+										"text_en",
+										"identity"
+									]
+								}
+							}
+						},
+						"_citation": {
+							"fields": {
+								"iso_639_3_eng": {
+									"analyzers": [
+										"text_en",
+										"identity"
+									]
+								}
+							}
+						},
+						"species_list": {
+							"analyzers": [
+								"text_en",
+								"identity"
+							]
+						}
+					},
+					"includeAllFields": false,
+					"storeValues": "id",
+					"trackListPositions": false
+				}
+			}
+		}
+	}
 }
 
 ///
